47d6034366d2bbd2965391eea46f11bca174e0b2
“jiminpark01” <bssjm17@gmail.com>
2023-01-09 16:20:30 +0900

    feat: Generate answer & Set username and guess
    I wrote this process to test python.
    Using randint from random library,
    generate random integer value from 1 to 100.

---
+++ numguess.py
@@ -0,0 +1,13 @@
+from random import randint
+
+# Generate answer
+answer = randint(1, 100)
+
+# User guess
+username = input("What's your name? ")
+print(f"Hello, {username}!")
+
+guess = input(f"{username}, Guess the number(1-100)!")
+print(f"Your guess is {guess}, right?")
+
+

b60435a93f47fea02f7921cf61bb35c0e40180d2
“jiminpark01” <bssjm17@gmail.com>
2023-01-11 16:48:16 +0900

    feat: Repeat the number of game attempts
    게임 시도 횟수 지정 및 반복 / 함수 만들기

--- numguess.py
+++ numguess.py
@@ -1,6 +1,18 @@
 from random import randint
 from time import sleep
 
+def play(N, guess, answer):
+    for i in range(N):
+        if guess == answer:
+            return 1
+        elif guess > answer:
+            print(f'That was too high, {username}.. Try again!')
+            guess = int(input("Guess the number(1-100)! "))
+        else:
+            print(f'That was too low, {username}.. Try again!')
+            guess = int(input("Guess the number(1-100)! "))
+
+
 # Generate answer
 answer = randint(1, 100)
 print(answer)
@@ -8,23 +20,17 @@ print(answer)
 # User guess
 username = input("What's your name? ")
 print(f"Hello, {username}!")
-guess = int(input(f"{username}, Guess the number(1-100)!"))
+guess = int(input(f"{username}, You have 7 chances. Guess the number(1-100)! "))
 print(f"Your guess is {guess}, right?")
 
 
 # result
-if guess == answer:
-    print("**********3**********")
-    sleep(1)
-    print("**********2**********")
-    sleep(1)
-    print("**********1**********")
+win = play(6, guess, answer)
+
+if win == 1:
+    print("Hmm...")
     sleep(1)
     print("That's right!! The answer is {}! Congratulations~!~!".format(answer))
-
-elif guess > answer:
-    print(f'Keep going, man~! That was too high, {username}..')
-
-elif guess < answer:
-    print(f'Keep going, man~! That was too low, {username}..')
+else:
+    print("----GAME OVER----")
 
